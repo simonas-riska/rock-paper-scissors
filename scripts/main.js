a961ddb3531a4c90d95164f282efbf2c45f4d900
Simonas Riska <simonas.riska@gmail.com>
2021-02-10 18:30:30 +0200

    Add game over condition

--- scripts/main.js
+++ scripts/main.js
@@ -9,67 +9,89 @@ function computersChoice() {
 
 let playerPoints = 0;
 let computerPoints = 0;
+let gameEnded = false;
 
 document.getElementById("rock").addEventListener("click", playerChoosesRock);
 document.getElementById("paper").addEventListener("click", playerChoosesPaper);
 document.getElementById("scissors").addEventListener("click", playerChoosesScissors);
 
 function playerChoosesRock() {
-    let computersSign = parseInt(computersChoice());
-    document.getElementById("player-status").innerHTML = "Your choice: <br><mark class=\"red\">Rock</mark>"
+    if (!gameEnded) {
+        let computersSign = parseInt(computersChoice());
+        document.getElementById("player-status").innerHTML = "Your choice: <br><mark class=\"red\">Rock</mark>"
 
-    if (computersSign == 2) {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Scissors</mark>"
-        playerPoints++;
-        document.getElementById("player-score").innerHTML = playerPoints;
-        document.getElementById("status-message").innerHTML = "You won!";
-    } else if (computersSign == 1) {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Paper</mark>"
-        computerPoints++;
-        document.getElementById("enemy-score").innerHTML = computerPoints;
-        document.getElementById("status-message").innerHTML = "You lost!";
-    } else {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Rock</mark>"
-        document.getElementById("status-message").innerHTML = "It's a tie!";
-    } 
+        if (computersSign == 2) {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Scissors</mark>"
+            playerPoints++;
+            document.getElementById("player-score").innerHTML = playerPoints;
+            document.getElementById("status-message").innerHTML = "You won!";
+        } else if (computersSign == 1) {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Paper</mark>"
+            computerPoints++;
+            document.getElementById("enemy-score").innerHTML = computerPoints;
+            document.getElementById("status-message").innerHTML = "You lost!";
+        } else {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Rock</mark>"
+            document.getElementById("status-message").innerHTML = "It's a tie!";
+        } 
+    }
+    gameOver();
 }
 
 function playerChoosesPaper() {
-    let computersSign = parseInt(computersChoice());
-    document.getElementById("player-status").innerHTML = "Your choice: <br><mark class=\"red\">Paper</mark>"
+    if (!gameEnded) {
+        let computersSign = parseInt(computersChoice());
+        document.getElementById("player-status").innerHTML = "Your choice: <br><mark class=\"red\">Paper</mark>"
 
-    if (computersSign == 0) {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Rock</mark>"
-        playerPoints++;
-        document.getElementById("player-score").innerHTML = playerPoints;
-        document.getElementById("status-message").innerHTML = "You won!";
-    } else if (computersSign == 2) {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Scissors</mark>"
-        computerPoints++;
-        document.getElementById("enemy-score").innerHTML = computerPoints;
-        document.getElementById("status-message").innerHTML = "You lost!";
-    } else {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Paper</mark>"
-        document.getElementById("status-message").innerHTML = "It's a tie!";
+        if (computersSign == 0) {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Rock</mark>"
+            playerPoints++;
+            document.getElementById("player-score").innerHTML = playerPoints;
+            document.getElementById("status-message").innerHTML = "You won!";
+        } else if (computersSign == 2) {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Scissors</mark>"
+            computerPoints++;
+            document.getElementById("enemy-score").innerHTML = computerPoints;
+            document.getElementById("status-message").innerHTML = "You lost!";
+        } else {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Paper</mark>"
+            document.getElementById("status-message").innerHTML = "It's a tie!";
+        }
     }
+    gameOver();
 }
 
 function playerChoosesScissors() {
-    let computersSign = parseInt(computersChoice());
-    document.getElementById("player-status").innerHTML = "Your choice: <br><mark class=\"red\">Scissors</mark>"
+    if (!gameEnded) {
+        let computersSign = parseInt(computersChoice());
+        document.getElementById("player-status").innerHTML = "Your choice: <br><mark class=\"red\">Scissors</mark>"
 
-    if (computersSign == 1) {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Paper</mark>"
-        playerPoints++;
-        document.getElementById("player-score").innerHTML = playerPoints;
-        document.getElementById("status-message").innerHTML = "You won!";
-    } else if (computersSign == 0) {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Rock</mark>"
-        computerPoints++;
-        document.getElementById("enemy-score").innerHTML = computerPoints;
-        document.getElementById("status-message").innerHTML = "You lost!";
-    } else {
-        document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Scissors</mark>"
-        document.getElementById("status-message").innerHTML = "It's a tie!";
+        if (computersSign == 1) {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Paper</mark>"
+            playerPoints++;
+            document.getElementById("player-score").innerHTML = playerPoints;
+            document.getElementById("status-message").innerHTML = "You won!";
+        } else if (computersSign == 0) {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Rock</mark>"
+            computerPoints++;
+            document.getElementById("enemy-score").innerHTML = computerPoints;
+            document.getElementById("status-message").innerHTML = "You lost!";
+        } else {
+            document.getElementById("enemy-status").innerHTML = "Computer's choice: <br><mark class=\"red\">Scissors</mark>"
+            document.getElementById("status-message").innerHTML = "It's a tie!";
+        }
+    }
+    gameOver();
+}
+
+function gameOver() {
+    if (playerPoints == 3) {
+        document.getElementById("status-message").innerHTML = "Victory!";
+        document.getElementById("play-again-message").innerHTML = "Play again?";
+        gameEnded = true;
+    } else if (computerPoints == 3) {
+        document.getElementById("status-message").innerHTML = "Defeat!";
+        document.getElementById("play-again-message").innerHTML = "Want a rematch?";
+        gameEnded = true;
     }
 }
